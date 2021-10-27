ab10565389874483c5fd38e9a40e08803303b1c1
Hans Reygaert <HansRDev@outlook.com>
2021-10-27 10:53:32 +0200

    new spring boot application

---
+++ src/main/java/com/hansreygaert/switchfully/euder/EuderApplication.java
@@ -0,0 +1,13 @@
+package com.hansreygaert.switchfully.euder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EuderApplication.class, args);
+	}
+
+}
